97f32b4f5284821b94119dd8ab67d3d9a9ed3024
Amir <assadollah.aminilari@gmail.com>
2015-12-31 13:52:35 -0800

    more changes to the project

--- gitSimpleProjectTest.java
+++ gitSimpleProjectTest.java
@@ -1,6 +1,11 @@
 public class gitSimpleProjectTest{
 	static public void main(String args[])
 	{	
-	System.out.print ("Hello World!!");
+	System.out.println ("This is github test!!");
+	/* Add some more lines*/
+	int a = 10;
+	int b = 20;
+	int c = a +b;
+	System.out.println("C = " + c);
 	}
 } 
