9f0fe0f982c616cfb585b7f01343c7b82d07098d
Amir <assadollah.aminilari@gmail.com>
2016-01-01 16:48:00 -0800

    one more update
    updated and want to commit and sync

--- gitSimpleProjectTest.java
+++ gitSimpleProjectTest.java
@@ -2,6 +2,7 @@ public class gitSimpleProjectTest{
 	static public void main(String args[])
 	{	
 	System.out.println ("Hello World!!");
+	System.out.println ("Hello World!!");
 	System.out.println ("This is github test!!");
 	/* Add some more lines*/
 	int a = 10;
