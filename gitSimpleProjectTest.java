bfe733dd1f7b6ae64c138fbc97b1b936bb918059
Amir <assadollah.aminilari@gmail.com>
2016-01-01 16:46:30 -0800

    Revert "Change to a line"
    This reverts commit d0d4d9dc601b8adb4d8934cbb6cc13c98c500696.

--- gitSimpleProjectTest.java
+++ gitSimpleProjectTest.java
@@ -1,7 +1,6 @@
 public class gitSimpleProjectTest{
 	static public void main(String args[])
 	{	
-	System.out.println ("Hello World!!");
 	System.out.println ("This is github test!!");
 	/* Add some more lines*/
 	int a = 10;
